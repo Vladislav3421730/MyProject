c979e48270a35cc115b604e0e38f59a461d40613
Владислав <vladpanasik674@gmail.com>
2023-09-07 21:15:31 +0300

    commit

--- src/main/java/com/bank/myproject/models/User.java
+++ src/main/java/com/bank/myproject/models/User.java
@@ -18,20 +18,25 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
+
     @Column(name="name")
     @NotEmpty
     @Size(min = 3)
     private String name;
+
     @Column(name="surname")
     @Size(min=3)
     @NotEmpty
     private String surname;
+
     @Min(18)
     @Column(name="age")
     private int age;
+
     @Min(1200)
     @Column(name="money")
     private int money;
+
     @ManyToOne(cascade =CascadeType.REFRESH,fetch = FetchType.LAZY)
     @JoinColumn(name="bank_id")
     private Bank bank;
