bb5a2f873cc0f70171d54d067788c87f6e584656
Vladislav3421730 <vladpanasik674@gmail.com>
2024-01-05 23:03:32 +0300

    Final Project 2

--- src/main/java/com/bank/myproject/controllers/MyRestController.java
+++ src/main/java/com/bank/myproject/controllers/MyRestController.java
@@ -1,19 +1,23 @@
 package com.bank.myproject.controllers;
 
+import com.bank.myproject.models.Bank;
 import com.bank.myproject.models.Client;
 import com.bank.myproject.models.User;
-import com.bank.myproject.services.ClientService;
+import com.bank.myproject.services.BankService;
 import com.bank.myproject.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@PreAuthorize("hasRole('ADMIN')")
 public class MyRestController {
 
     private final UserService userService;
+    private final BankService bankService;
     @GetMapping("/users")
     public List<User> GetAllUsers(){
         List<User> list =userService.AllUsers();
